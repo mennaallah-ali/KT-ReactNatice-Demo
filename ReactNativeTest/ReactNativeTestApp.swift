54f7a2bd513fbfe0a16e6458eb7a581cd9f7df85
mennaallah-ali <mennaallah.ali@vodafone.com>
2024-08-11 15:32:39 +0300

    Initial Commit

---
+++ ReactNativeTest/ReactNativeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactNativeTestApp.swift
+//  ReactNativeTest
+//
+//  Created by Menna Omar on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReactNativeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
